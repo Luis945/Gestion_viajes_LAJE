abfad573ae2b1586d6437c5d0776960493ae3b24
Luis945 <luis_aguirre_1994@outlook.com>
2018-04-11 14:31:47 -0500

    agregué fragments

--- app/src/main/java/com/example/luis/gestion_viajes/adaptadores/viajesAdapter.java
+++ app/src/main/java/com/example/luis/gestion_viajes/adaptadores/viajesAdapter.java
@@ -1,31 +1,66 @@
 package com.example.luis.gestion_viajes.adaptadores;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.luis.gestion_viajes.R;
+import com.example.luis.gestion_viajes.objetos.Viaje;
+
+import java.util.ArrayList;
 
 /**
  * Created by luis on 09/04/18.
  */
 
 public class viajesAdapter extends BaseAdapter {
+
+    ArrayList<Viaje> viajes;
+    Context context;
+
+    public viajesAdapter(ArrayList<Viaje> viajes, Context context) {
+        this.viajes = viajes;
+        this.context = context;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return viajes.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return viajes.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return viajes.get(position).getOperadora();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        Viaje viaje= viajes.get(position);
+        View v= convertView;
+        v= LayoutInflater.from(context).inflate(R.layout.viaje_mandado,null);
+
+        TextView fecha, telefono, direccion, unidad,operadora;
+        fecha= (TextView) v.findViewById(R.id.lbl_fecha);
+        telefono=(TextView) v.findViewById(R.id.lbl_teléfono);
+        direccion=(TextView) v.findViewById(R.id.lbl_direccion);
+        unidad=(TextView) v.findViewById(R.id.lbl_unidad);
+        operadora= (TextView) v.findViewById(R.id.lbl_operadora);
+        fecha.setText(viaje.getFecha());
+        telefono.setText(viaje.getTelefono());
+        direccion.setText(viaje.getDireccion());
+        unidad.setText(viaje.getUnidad());
+        operadora.setText(viaje.getOperadora());
+
+
+
+        return v;
     }
 }
