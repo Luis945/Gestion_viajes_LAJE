33981797c3924ba76d05a3426aec5b4c441feb4e
Luis945 <luis_aguirre_1994@outlook.com>
2018-04-11 14:45:36 -0500

    agregué fragments

--- app/src/main/java/com/example/luis/gestion_viajes/ventana.java
+++ app/src/main/java/com/example/luis/gestion_viajes/ventana.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 
 /**
@@ -48,6 +49,7 @@ public class ventana extends Fragment {
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
+
         return fragment;
     }
 
@@ -57,13 +59,17 @@ public class ventana extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
+        }
+
+
+    }
+    ListView listView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        listView= (ListView) getView().findViewById(R.id.listaViajes);
         return inflater.inflate(R.layout.fragment_ventana, container, false);
     }
 
