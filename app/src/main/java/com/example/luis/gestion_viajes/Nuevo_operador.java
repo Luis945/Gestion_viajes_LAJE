cc0d206ca638a4896b7eed61891d4deb7e9a31fc
angeloctaviopunk4 <octavioangelgg4@gmail.com>
2018-04-16 18:41:57 -0500

    cambio leve

--- app/src/main/java/com/example/luis/gestion_viajes/Nuevo_operador.java
+++ app/src/main/java/com/example/luis/gestion_viajes/Nuevo_operador.java
@@ -184,7 +184,9 @@ public class Nuevo_operador extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
+
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+
     }
 }
