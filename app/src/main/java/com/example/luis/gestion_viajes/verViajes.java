5f493f2ad2c4d4e441eae71159816e9b5ee7a208
Luis945 <luis_aguirre_1994@outlook.com>
2018-04-19 15:45:33 -0500

    inicio la vista de viajes mandados

--- app/src/main/java/com/example/luis/gestion_viajes/verViajes.java
+++ app/src/main/java/com/example/luis/gestion_viajes/verViajes.java
@@ -4,9 +4,26 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.luis.gestion_viajes.objetos.Viaje;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
 
 
 /**
@@ -17,7 +34,7 @@ import android.view.ViewGroup;
  * Use the {@link verViajes#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class verViajes extends Fragment {
+public class verViajes extends Fragment implements Response.Listener<JSONObject>,Response.ErrorListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -60,11 +77,64 @@ public class verViajes extends Fragment {
         }
     }
 
+    RadioButton radio_unidad,radio_telefono,radio_todos;
+    TextView buscar;
+    Button aceptar;
+    RecyclerView recyclerView;
+    String url="";
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ver_viajes, container, false);
+        View view =inflater.inflate(R.layout.fragment_ver_viajes, container, false);
+        radio_telefono= view.findViewById(R.id.radio_telefono);
+        radio_unidad= view.findViewById(R.id.radio_unidad);
+        radio_todos=view.findViewById(R.id.radio_todos);
+        buscar= view.findViewById(R.id.busqueda_txt);
+        aceptar= view.findViewById(R.id.busqueda_btn);
+        recyclerView=  view.findViewById(R.id.verViajesT);
+        if (radio_todos.isChecked()){
+            cargarViajes();
+        }
+        return view;
+    }
+    boolean cambio=false;
+    JSONObject object;
+    private void cargarViajes() {
+      if (radio_todos.isChecked()){
+          url="http://rtaxis.uttsistemas.com/verviajes";
+          try {
+              object.put("validacion","aceptar");
+          } catch (JSONException e) {
+              e.printStackTrace();
+          }
+          cambio=true;
+      }else if (radio_unidad.isChecked()){
+          url="http://rtaxis.uttsistemas.com/verviajesUnidad";
+          try {
+              object.put("unidad",buscar.getText().toString());
+          } catch (JSONException e) {
+              e.printStackTrace();
+          }
+          cambio=true;
+      }else if (radio_telefono.isChecked()){
+          url="http://rtaxis.uttsistemas.com/verviajesTelefono";
+          try {
+              object.put("telefono",buscar.getText().toString());
+          } catch (JSONException e) {
+              e.printStackTrace();
+          }
+          cambio=true;
+      }
+
+        if (cambio) {
+            JsonObjectRequest stringRequest1= new JsonObjectRequest(Request.Method.GET,url,object,this,this);
+            stringRequest1.setRetryPolicy(new DefaultRetryPolicy(6000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+            //  queue.add(jsonObjectRequest);
+            Volley.newRequestQueue(getContext()).add(stringRequest1);
+
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -89,6 +159,16 @@ public class verViajes extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+
+    }
+    List<Viaje> viajes;
+    @Override
+    public void onResponse(JSONObject response) {
+
     }
 
     /**
